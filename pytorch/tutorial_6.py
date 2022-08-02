49c1e2cd923d0212caa8bfcc31cce2cd5620809b
Hyunjae Kim <peterhyunjae@naver.com>
2022-08-02 13:33:59 +0900

    train model, test model

--- pytorch/tutorial_6.py
+++ pytorch/tutorial_6.py
@@ -87,3 +87,13 @@ def test_loop(dataloader, model, loss_fn):
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
